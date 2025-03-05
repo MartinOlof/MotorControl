e9772efa352dae4356db091b28ed996c64464d91
MartinOlof <94788540+MartinOlof@users.noreply.github.com>
2025-03-05 23:25:00 +0100

    Update motorControlPushButton.c

--- motorControlPushButton.c
+++ motorControlPushButton.c
@@ -5,30 +5,56 @@
 
 #define IN_A PORTB |= (1 << PB5)
 #define IN_B PORTB |= (1 << PB3)
-#define PWM PORTB |= (1 << PB0)
-#define CS PORTB |= (1 << PB1)
-#define SEL0 PORTB |= (1 << PB2)
-#define PUSH_BUTTON //PB4
+#define STOP_A PORTB &= ~(1 << PB5)
+#define STOP_B PORTB &= ~(1 << PB3)
+#define PUSH_BUTTON PB4
 
-uint_8t buttonCount = 0;
-
-// Push button connected PB1
-
-// have a count on push button up to 3, if button count == 1, close it, if button count == 2 open it, reset count after each interation
-
+volatile uint8_t buttonCount = 0;
 
 void motorControl();
+void initButtonInterrupt();
 
-int main(void){
-
-  while(1){
-  
-  }
+int main(void) {
+    // Configure PB5 and PB3 as outputs
+    DDRB |= (1 << PB5) | (1 << PB3);
+    
+    // Configure PB4 as input with pull-up resistor
+    DDRB &= ~(1 << PUSH_BUTTON);
+    PORTB |= (1 << PUSH_BUTTON);
+    
+    // Initialize button interrupt
+    initButtonInterrupt();
+    sei(); // Enable global interrupts
+    
+    while (1) {
+        // Main loop does nothing, all handled in ISR
+    }
 }
 
-void motorControl(){
-  if(buttonCount == 1){
-    
-  }
+void initButtonInterrupt() {
+    GIMSK |= (1 << PCIE);   // Enable pin change interrupt
+    PCMSK |= (1 << PUSH_BUTTON); // Enable interrupt for PB4
+}
 
+ISR(PCINT0_vect) {
+    if (!(PINB & (1 << PUSH_BUTTON))) { // Check if button is pressed
+        _delay_ms(50); // Simple debounce
+        if (!(PINB & (1 << PUSH_BUTTON))) {
+            buttonCount++;
+            if (buttonCount > 2) buttonCount = 1;
+            motorControl();
+        }
+    }
+}
+
+void motorControl() {
+    if (buttonCount == 1) {
+        // Extend actuator
+        IN_A;
+        STOP_B;
+    } else if (buttonCount == 2) {
+        // Retract actuator
+        IN_B;
+        STOP_A;
+    }
 }
